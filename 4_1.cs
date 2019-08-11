9f24b38e88292c4754a80cb3fa687d3dda32cd53
akovalenko2000 <49165936+akovalenko2000@users.noreply.github.com>
2019-08-12 00:28:14 +0300

    Add files via upload

---
+++ 4_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Square
+{
+    public double side;
+	public Square()
+	{
+	}
+    public Square(double side)
+    {
+        this.side = side;
+    }
+    public virtual double Perimeter(double side)
+    { return 4 * side; }
+    public virtual double Area(double side)
+    { return side * side; }
+}
---
+++ 4_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Arithm_pr
+{
+    private double first_number, difference;
+    public double nth_number, sum_of_n_numbers;
+    private int numbers;
+    public Arithm_pr(int numbers, double first_number, double difference)
+	{
+        this.first_number = first_number;
+        this.numbers = numbers;
+        this.difference = difference;
+        nth_number = member(first_number, difference, numbers);
+        sum_of_n_numbers = sum(first_number, difference, numbers);
+    }
+
+
+    public double member(double first_number, double difference, int numbers)
+    {
+        do
+        {
+            nth_number = member(first_number, difference, numbers - 1) + difference;
+        }
+        while (numbers > 1);
+        return nth_number;
+    }
+
+    public double sum(double first_number, double difference, int numbers)
+    {
+        do
+        {
+            sum_of_n_numbers = member(first_number, difference, numbers - 1) + sum(first_number, difference, numbers); //алгоритм взял навскидку, не силен в рекурсиях. Лучше поискать в справочниках
+        }
+        while (numbers > 1); 
+        return sum_of_n_numbers;
+    }
+}
